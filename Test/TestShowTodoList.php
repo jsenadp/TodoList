f371a33e93ae099ceaf20c6f0344d266440bec4a
Jalasena Dwi Putra <56987482+jsenadp@users.noreply.github.com>
2022-08-23 21:10:30 +0700

    Add files via upload

---
+++ Test/TestShowTodoList.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "../Model/ToDoList.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+
+$todolist[1] = "Belajar Php Dasar";
+$todolist[2] = "Belajar Php OOP";
+$todolist[3] = "Belajar Php Database";
+
+ShowTodoList();
