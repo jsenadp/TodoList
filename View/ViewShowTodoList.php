afcbc4af316f4a0492c5124f278c44daa5e27b55
Jalasena Dwi Putra <jalasenadp@gmail.com>
2022-08-31 21:07:42 +0700

    Update ViewShowTodoList.php

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,6 +1,32 @@
 <?php
 
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
+require_once "../Helper/input.php";
+
 function ViewShowTodoList(){
 
+    while (true) {
+        ShowTodoList();
+
+        echo "MENU".PHP_EOL;
+        echo "1. Tambah Todo".PHP_EOL;
+        echo "2. Hapus Todo".PHP_EOL;
+        echo "x. Keluar".PHP_EOL;
+
+        $pilihan = input("Pilih");
+        if ($pilihan == "1"){
+            viewAddTodoList();
+        }elseif ($pilihan == "2"){
+            viewRemoveTodoList();
+        }elseif ($pilihan == "x"){
+            break;
+        }else{
+            echo "Pilihan Tidak Tersedia";
+        }
+    }
+
+    echo "Sampai Jumpa Lagi";
     
 }
